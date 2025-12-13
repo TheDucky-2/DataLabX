886502369b57558bf77749e3b7b8a7620b04d4c2
TheDucky-2 <datalab.project@protonmail.com>
2025-12-13 18:34:43 +0530

    Updated the init_py in data_visualization package, to include Missingness Viz module

--- datalab/tabular/data_visualization/__init__.py
+++ datalab/tabular/data_visualization/__init__.py
@@ -1,4 +1,7 @@
 from .DataVisualizer import DataVisualizer
 from .CategoricalVisualizer import CategoricalVisualizer
+from .NumericalVisualizer import NumericalVisualizer
+from .MissingnessVisualizer import MissingnessVisualizer
 
-__all__ = ['DataVisualizer', 'CategoricalVisualizer']
+
+__all__ = ['DataVisualizer', 'CategoricalVisualizer', 'NumericalVisualizer', 'MissingnessVisualizer']
