7842441bb81bb956f3073331f0d620c8131383a6
TheDucky-2 <datalab.project@protonmail.com>
2026-01-09 00:10:40 +0530

    Updated Text Diagnosis to only include detect_splitters() and detect_empty_strings() methods and set extra column called 'Index' during polars-> pandas conversion to preserve row ids

--- datalab/tabular/data_diagnosis/TextDiagnosis.py
+++ datalab/tabular/data_diagnosis/TextDiagnosis.py
@@ -1,9 +1,9 @@
 from ..utils import BackendConverter
 
-from pathlib import Path
-
 import pandas as pd
 import polars as pl
+
+import polars.selectors as cs
 
 class TextDiagnosis():
 
@@ -52,100 +52,31 @@ class TextDiagnosis():
 			    Returns all rows with empty_strings
         
         '''
+        # converting pandas -> polars
         polars_df = BackendConverter(self.df).pandas_to_polars()
 
-        empty_string_df = polars_df.filter(pl.any_horizontal([(pl.col(column) == "") for column in polars_df.columns]))
+        # setting an extra column called 'Index' to preserve row id.
+        polars_df = polars_df.with_row_index('Index')
+        
+        # checking for empty string in any column  while also excluding 'Index' column
+        empty_string_df = polars_df.filter(pl.any_horizontal(cs.exclude('Index').str.len_chars() == 0))
 
+        # converting back to Pandas
         empty_string_df = BackendConverter(empty_string_df).polars_to_pandas()
+
+        # setting 'Index' column as the index to preserve row id
+        empty_string_df.index = empty_string_df['Index']
+
+        # removing the extra 'Index' column
+        empty_string_df.drop(columns='Index', inplace=True)
         
         return empty_string_df
-
-    def detect_whitespaces(self) -> pd.DataFrame:
-        '''
-        Filters rows with whitespaces for each column of Text DataFrame (string, object or category type columns).
-        
-        Return:
-            df : pd.DataFrame
-            A pandas DataFrame of rows with whitespaces or simply spaces, for values.
-        
-        Usage Recommendation:
-            Use this function when you want to diagnose rows with whitespaces during text diagnosis.
-
-        Considerations:
-            This function uses polars's filter method to filter out rows where value is a whitespace (" ").
-
-        Example: 
-            TextDiagnosis(df).detect_whitespaces()
-            ->
-                Returns all rows with white spaces
-        
-        '''
-        polars_df = BackendConverter(self.df).pandas_to_polars()
-
-        whitespace_df = polars_df.filter(pl.any_horizontal([(pl.col(column) == " ") for column in polars_df.columns]))
-
-        whitespace_df = BackendConverter(whitespace_df).polars_to_pandas()
-            
-        return whitespace_df
-
-    def detect_multiple_spaces(self) -> pd.DataFrame:
-
-        '''
-        Detects multiple spaces within characters or words in each column of the Text DataFrame.
-
-        Returns:
-            pd.DataFrame
-            A pandas DataFrame of rows with text having multiple spaces.
-
-        Usage Recommendation:
-        ---------------------
-            1. Use this function when you want to detect multiple spaces inside text.
-            2. Use this for diagnosing text before replacing multiple spaces with TextCleaner(df).replace_multiple_spaces_with_single()
-
-        Considerations:
-        --------------
-            Checks two consecutive spaces under the hood.
-
-        Example:
-            TextDiagnosis(df).detect_multiple_spaces()
-        '''
-        polars_df = BackendConverter(self.df).pandas_to_polars()
-
-        multiple_spaces_df = polars_df.filter(pl.any_horizontal([pl.col(c).str.contains(r'\s{2,}') for c in polars_df.columns]))
-        
-        multiple_spaces_df = BackendConverter(multiple_spaces_df).polars_to_pandas()
-        
-        return multiple_spaces_df
-
-    def detect_dots_within_text(self)-> pd.DataFrame:
-        '''
-        Detects dots within words or strings for one or multiple columns of the DataFrame.
-
-        Returns:
-            pd.DataFrame
-            A pandas DataFrame of rows with one or more dots within text.
-
-        Usage Recommendation:
-            Use this function when you want to detect if there are dots within text.
-
-        Consideration:
-            Use this function for exploring data before replacing dots with TextCleaner(df).replace_dots_within_text()
-
-        Example:
-            TextDiagnosis(df).detect_dots_within_text()
-
-        '''
-        polars_df = BackendConverter(self.df).pandas_to_polars()
-        
-        text_with_dots_df = polars_df.filter(pl.any_horizontal(pl.col(pl.Utf8).str.contains(r'\.')))
-
-        text_with_dots = BackendConverter(text_with_dots_df).polars_to_pandas()
-
-        return text_with_dots
 
     def detect_splitters(self, splitters = [","]):
 
         polars_df = BackendConverter(self.df).pandas_to_polars()
+
+        polars_df = polars_df.with_row_index('Index')
 
         # joining splitters to convert them into a regex pattern for detecting splitters
         joined_splitters=f'[{"".join(splitters)}]+'
@@ -157,6 +88,11 @@ class TextDiagnosis():
 
         df = BackendConverter(splitters_df).polars_to_pandas()
 
+        df.index = df['Index']
+
+        df.drop(columns='Index', inplace=True)
+        
         return df
+
 
 
