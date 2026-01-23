1fd3101c195ed6c646da9c9ed39caad9bb5127b1
TheDucky-2 <datalab.project@protonmail.com>
2026-01-23 22:23:24 +0530

    Removed DtypeConverter for now

--- datalab/tabular/data_cleaner/__init__.py
+++ datalab/tabular/data_cleaner/__init__.py
@@ -1,10 +1,9 @@
 from .NumericalCleaner import NumericalCleaner
 from .MissingHandler import MissingHandler
 from .ColumnConverter import ColumnConverter
-from .DtypeConverter import DtypeConverter
 from .TextCleaner import TextCleaner
 
 
-__all__ = [ 'ColumnConverter','DtypeConverter', 'NumericalCleaner', 'MissingHandler', 'TextCleaner']
+__all__ = [ 'ColumnConverter', 'NumericalCleaner', 'MissingHandler', 'TextCleaner']
 
 
