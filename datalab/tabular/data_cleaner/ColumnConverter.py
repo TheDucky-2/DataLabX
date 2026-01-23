5988479436df770d80aff105c45a50749e65f4fc
TheDucky-2 <datalab.project@protonmail.com>
2026-01-23 20:04:22 +0530

    Updated the code and docstrings. Also fixed formatting issues

--- datalab/tabular/data_cleaner/ColumnConverter.py
+++ datalab/tabular/data_cleaner/ColumnConverter.py
@@ -1,22 +1,23 @@
+"""Converts columns to Numerical, Categorical or Datetime type"""
+
 import pandas as pd
+from ..utils.Logger import datalab_logger
+
+logger = datalab_logger(name = __name__.split('.')[-1])
 
 class ColumnConverter:
     def __init__(self, df:pd.DataFrame, columns: list = None):
-        ''' 
-        Initializing the ColumnConverter
-        
-        Parameters:
+        """
+        Initializing the ColumnConverter.
+
+        Parameters
         -----------
-        df       : pd.DataFrame 
-            A pandas DataFrame 
+        df : pd.DataFrame
+            A pandas DataFrame.
 
-        columns  : list or type(None)
-            List of columns to convert into numerical columns. E.g: ['Quantity', 'Price', 'Number of Orders']
-
-        **kwargs : dict
-            A dict of extra keyword arguments you may want to pass
-        '''
-
+        columns : list or type(None)
+            List of columns you wish to convert, by default None.
+        """
         if not isinstance(df, pd.DataFrame):
             raise TypeError(f'df must be pandas DataFrame, got {type(df).__name__}')
 
@@ -26,260 +27,254 @@ class ColumnConverter:
         self.df = df
 
         if columns is None:
-            self.columns = df.columns.tolist()
+            self.columns = self.df.columns.tolist()
         else:
-            self.columns = columns
+            self.columns = [column for column in columns if column in self.df.columns]
 
-        print(f'ColumnConverter initialized with columns: {self.columns}')
+        logger.info(f'ColumnConverter initialized.')
 
-    
-    def to_datetime(self, inplace: bool=False, dayfirst=False) -> pd.DataFrame:
-        '''
+    def to_datetime(self, inplace: bool=False, dayfirst=False, **kwargs) -> pd.DataFrame:
+        """
         Convert one or more columns column into datetime columns.
 
-        Parameters:
+        Parameters
         -----------
-            self: pd.DataFrame
-                A pandas DataFrame
+        inplace  : bool, optional
+            Whether you want to apply changes to dataframe the original dataframe in place, by default False.
 
-            Optional:
-
-            inplace  : bool (default is False)
-                Whether you want to apply changes to dataframe. 
-
-            dayfirst : bool (default is False)
-                Whether you would like day to appear first or months to appear first, in dates.
+        dayfirst : bool, optional
+            Whether the order of days is to be set before months in dates, by default False.
         
-        Returns:
+        kwargs: dict, optional
+            A dictionary of keyword arguments you wish to pass in ``pd.to_datetime()`` method.
+
+        Returns
         --------
+        pd.DataFrame
             A pandas DataFrame 
 
-            1. return the original dataframe with converted datetime columns, if inplace=True
-            2. return only the dataframe of converted datetime columns with rest of the DataFrame unchanged, if default.-
+            1. Makes changes to the original dataframe in place, but returns None, if inplace=True.
+            2. Returns the dataframe of converted datetime columns with rest of the non-datetime columns, if inplace = False.
 
-        Usage Recommendation:
+        Usage Recommendation
         ---------------------
-            Use this function to convert columns into datetime columns for extracting date and time later
+            Use this function to convert columns into datetime columns for working with date and time data later.
 
-        Considerations:
+        Considerations
         ---------------
-            This function converts keeps the values that cannot be converted as they are.
+            This function keeps non-converted values as they are.
 
-        Examples:
+        Example
+        --------
+        >>> ColumnConverter(df).to_datetime()
         
-        >>>> to_numerical(df, ['Quantity', 'Price Per Unit', 'Total Spent'], inplace=True]) 
-        #   returns full original dataframe with converted columns
-
-        >>>> to_numerical(df, ['Quantity', 'Price Per Unit', 'Total Spent'])
-        #   returns dataframe of numeric columns ['Quantity', 'Price Per Unit', 'Total Spent'] 
-
-        '''
-
+        >>> ColumnConverter(df).to_datetime(inplace=True) 
+        """
         if not isinstance(inplace, bool):
             raise TypeError(f'inplace must be a boolean: True or False, got {type(inplace).__name__}')
-        # we will be creating a function to convert each value to datetime if valid, else we will leave it as it is.
 
         if not isinstance(dayfirst, bool):
             raise TypeError(f'dayfirst must be True or False, got {type(dayfirst).__name__}')
 
-        df_copy = self.df.copy()
+        if not isinstance(kwargs, dict):
+            raise TypeError(f'keyword arguments must be a dict, got {type(kwargs).__name__}')
 
-        for column in df_copy[self.columns]:
+        for column in self.df[self.columns]:
             
             if dayfirst:
                 # apply datetime conversion to all columns passed
-                converted_to_datetime = pd.to_datetime(df_copy[column], dayfirst=True, errors = 'coerce')
+                converted_to_datetime = pd.to_datetime(self.df[column], dayfirst=True, errors = 'coerce', **kwargs)
             
             else:
-                converted_to_datetime = pd.to_datetime(df_copy[column], dayfirst=False,errors='coerce')
-                
+                converted_to_datetime = pd.to_datetime(self.df[column], dayfirst=False,errors='coerce', **kwargs)
+            
+            # values where conversion can be done will stay True
             conversion_successful = converted_to_datetime.notna()
-            # now check scenarios where conversion to datetime failed but is not invalid value in the DataFrame
-            conversion_failed = converted_to_datetime.isna() & df_copy[column].notna()
+
+            # we want values where failed conversion resulted into NA, however, those values were not NA in original dataframe
+            conversion_failed = converted_to_datetime.isna() & self.df[column].notna()
 
             # if conversion to datetime does not fail
             if not conversion_failed.any():
 
                 # assign converted values to the whole columns of the DataFrame
-                df_copy[column] = converted_to_datetime
+                self.df[column] = converted_to_datetime
             else:
                 if conversion_successful.any():
                     # if conversion fails, only convert correct datetime values and leave others as they are
-                    df_copy.loc[converted_to_datetime.notna(), column] = converted_to_datetime[converted_to_datetime.notna()]
+                    self.df.loc[converted_to_datetime.notna(), column] = converted_to_datetime[converted_to_datetime.notna()]
 
         if inplace:
             return None
             
         else:
-            return df_copy
+            return self.df
 
     def to_categorical(self,inplace: bool=False, **kwargs)-> pd.DataFrame:
-        '''
-        Convert a column to a Categorical column
+        """
+        Convert a column to a Categorical column.
 
-        Parameters:
-            df      : pd.DataFrame | str | Path 
-            A pandas DataFrame or a file Path. E.g: (pd.DataFrame or 'example.csv')
+        Parameters
+        -----------
+        inplace  : bool (default is False)
+            Making changes to the original dataframe. E.g: True or False.
 
-            columns : list[str]
-            A list of one or more column names.Example: ['Item', 'Payment Method', 'Location']
+        kwargs: dict
+            A dictionary of keyword arguments you wish to pass in ``pd.Categorical()`` method.
 
-            inplace  : bool (default is False)
-            Making changes to the original dataframe. E.g: True or False
-
-            **kwargs: dict
             Optional keyword arguments:
-            - categories : list
-                If passed, it will keep only the category names passed in, and will convert everything else to NAN. E.g: ['Great','Good','Better','OK']
-            - ordered    : bool , default False
-                Whether the categories need to have an order or not. E.g: (Great > Good > Better > OK) or (1 < 2 < 3 < 4 < 5)
 
-        Return:
-            pd.DataFrame
-            1. Return a pandas DataFrame with columns converted as Categoricals, if 'categories' and 'ordered' passed as parameters.
-            2. Return a pandas DataFrame with updated columns with dtype 'category', if parameters not passed.
+            - categories : list, optional, default None
+                If passed, it will keep only the values passed in, and will convert everything else to NAN. E.g: ['Great','Good','Better','OK'].
 
-        Usage Recommendation:
+            - ordered    : bool, optional, default False
+                Whether the categories need to have an order or not, E.g: (Great > Good > Better > OK) or (1 < 2 < 3 < 4 < 5).
+
+        Returns
+        ------
+        pd.DataFrame
+            A pandas DataFrame
+
+            1. Return a pandas DataFrame with columns converted as Categorical, if 'categories' and 'ordered' passed as parameters.
+            2. Return a pandas DataFrame with updated columns with 'category' dtype, if parameters not passed.
+
+        Usage Recommendation
+        ---------------------
             1. Use this function when there aren't many unique values in your string columns. 
-            2. Do not use this function for columns that do not require you to convert text to integers for encoding later. ['Email', 'Name', 'Transaction ID']
+            2. DO NOT Use this function for text columns that you would not be converting to integers, E.g: ['Email', 'Name', 'Transaction ID'].
 
-        Considerations: 
+        Considerations
+        ---------------
             If in doubt whether a column has to be converted to categorical or not, leave the column as an object or string until required.
 
-        Examples:
-        >>>     to_categorical(df, ['Item', 'Payment Method', 'Ratings'], inplace=True) 
-        #       returns full original dataframe with converted columns
+        Example
+        --------
+        >>>  ColumnConverter(df).to_categorical(inplace=True) 
 
-        >>>     to_categorical(df, ['Ratings'], categories= ['Great','Good','Better','OK'], ordered=True)
-        #       returns the pandas Series with converted column having only the values passed as categories, and everything else converted to NAN
-        '''
-
+        >>>  ColumnConverter(df).to_categorical(categories= ['Great','Good','Better','OK'], ordered=True)
+        """
         if not isinstance(inplace, bool):
             raise TypeError(f'inplace must be True or False, got {type(inplace).__name__}')
+            
+        if not isinstance(kwargs, dict):
+            raise TypeError(f'keyword arguments must be a dict, got {type(kwargs).__name__}')
 
         categories = kwargs.get('categories', None)
         ordered = kwargs.get('ordered', False)
 
-        # created a copy of the data that we will be working on, to assign to self.df if inplace = True later
-        df_copy = self.df if inplace else self.df.copy()
+        for column in self.df[self.columns]:
 
-        for column in self.columns:
             if categories is not None:
-                df_copy[column] = pd.Categorical(df_copy[column], categories=categories, ordered = ordered)
+                self.df[column] = pd.Categorical(self.df[column], categories=categories, ordered = ordered)
 
             else:
-                df_copy[column] = df_copy[column].astype('category')
+                self.df[column] = self.df[column].astype('category')
 
         if inplace:
-            self.df = df_copy
             return None
             
         else:
-            return df_copy
+            return self.df
         
-    def to_numerical(self, inplace:bool=False) -> pd.DataFrame:
-        '''
-        Convert one or more columns column into numerical columns.
+    def to_numerical(self, inplace:bool=False, **kwargs) -> pd.DataFrame:
+        """
+        Convert one or more columns into numerical columns.
 
-        Parameters:
+        Parameters
         -----------
-        inplace  : bool
-            Making changes to the original dataframe. E.g: True or False
+        inplace  : bool, optional
+            Making changes to the original dataframe, by default False.
 
-        Returns:
+        Returns
         --------
         pd.DataFrame
             A pandas DataFrame 
-                1. return the original dataframe with converted numerical columns, if inplace=True
-                2. return only the dataframe of converted numerical columns, if inplace=False
+                1. Makes changes to the original dataframe in place, but returns None, if inplace=True
+                2. Returns the dataframe of converted numerical columns with rest of the non-numerical columns, if inplace = False.
 
-        Usage Recommendation:
+        Usage Recommendation
         ---------------------
             Use this function to convert columns into numeric values for later calculations.
         
-        Considerations:
+        Considerations
         ---------------
-            This function converts non-convertible values to NaN and then restores those values back from NaN to original.
+            This function keeps non-convertible values as they are, but columns may remain of 'object' dtype in case numbers are dirty.
 
-        Examples:
+        Example
+        --------
         
-        >>> to_numerical(df, ['Quantity', 'Price Per Unit', 'Total Spent'], inplace=True]) 
-        #   returns full original dataframe with converted columns
+        >>> ColumnConverter(df).to_numerical(inplace=True) 
 
-        >>> to_numerical(df, ['Quantity', 'Price Per Unit', 'Total Spent'])
-        #   returns dataframe of numeric columns ['Quantity', 'Price Per Unit', 'Total Spent']
-        ''' 
-
+        >>> ColumnConverter(df).to_numerical()
+        """ 
         if not isinstance(inplace, bool):
             raise TypeError(f'inplace must be True or False, got {type(inplace).__name__}')
 
-        if inplace:
-            original_columns = self.df[self.columns].copy()
+        if not isinstance(kwargs, dict):
+            raise TypeError(f'keyword arguments must be a dict, got {type(kwargs).__name__}')
+
+        original_columns = self.df[self.columns].copy()
             
-            # first converting all data to numerical, and non-numerical get converted to NaN
-            self.df[self.columns] = self.df[self.columns].apply(pd.to_numeric, errors = 'coerce')
+        # first converting all data to numerical, and non-numerical get converted to NaN
+        self.df[self.columns] = self.df[self.columns].apply(pd.to_numeric, errors = 'coerce', **kwargs)
 
-            #  reverting NaN values back to original missing values
-            self.df[self.columns] = self.df[self.columns].combine_first(original_columns)
+        #  reverting NaN values back to original missing values
+        self.df[self.columns] = self.df[self.columns].combine_first(original_columns)
+
+        if inplace:
             return None
-
         else:
-            df_copy = self.df.copy()
-            # first converting all data to numerical, and non-numerical get converted to NaN
-            df_copy[self.columns] = self.df[self.columns].apply(pd.to_numeric, errors='coerce')
+            return self.df
 
-            #  reverting NaN values back to original missing values
-            df_copy[self.columns] = df_copy[self.columns].combine_first(self.df[self.columns])
-            return df_copy
-
-    def to_numerical_forced(self, inplace:bool=False)-> pd.DataFrame:
-        '''
+    def to_numerical_forced(self, inplace:bool=False, **kwargs)-> pd.DataFrame:
+        """
         Convert one or more columns column into numerical columns forcibly as it turns non-convertible values to NaN.
 
-        Parameters:
+        Parameters
         -----------
-        inplace  : bool
-            Making changes to the original dataframe. E.g: True or False
+        inplace  : bool, optional
+            Making changes to the original dataframe, by default False.
 
-        Returns:
+        kwargs: dict, optional
+            A dictionary of keyword arguments you wish to pass in ``pd.to_numeric()`` method.
+
+        Returns
         --------
         pd.DataFrame
             A pandas DataFrame 
-                1. return the original dataframe with converted numerical columns, if inplace=True
-                2. return only the dataframe of converted numerical columns, if inplace=False
 
-        Usage Recommendation:
+                1. Makes changes to the original dataframe in place, but returns None, if inplace=True
+                2. Returns the dataframe of converted numerical columns with rest of the non-numerical columns, if inplace = False.
+
+        Usage Recommendation
         ---------------------
             1. Use this function to convert columns into numeric datatype for later calculations.
         
-        Considerations:
+        Considerations
         ---------------
-            1. This function converts non-convertible values to NaN.
+            This function converts non-convertible values to NaN, and forces conversion to numeric dtype.
 
-        Examples:
-        
-        >>> ColumnConverter(df).to_numerical_forced(df, ['Quantity', 'Price Per Unit', 'Total Spent'], inplace=True]) 
-        #   returns full original dataframe with converted columns
+        Example
+        --------
 
-        >>> ColumnConverter(df).to_numerical_forced(df, ['Quantity', 'Price Per Unit', 'Total Spent'])
-        #   returns dataframe of numeric columns ['Quantity', 'Price Per Unit', 'Total Spent']
+        >>> ColumnConverter(df).to_numerical_forced(inplace=True) 
 
-        ''' 
+        >>> ColumnConverter(df).to_numerical_forced()
+
+        """ 
         if not isinstance(inplace, bool):
             raise TypeError(f'inplace must be True or False, got {type(inplace).__name__}')
+            
+        if not isinstance(kwargs, dict):
+            raise TypeError(f'keyword arguments must be a dict, got {type(kwargs).__name__}')
+
+        # first converting all data to numerical, and non-numerical get converted to NaN
+        self.df[self.columns] = self.df[self.columns].apply(pd.to_numeric, errors = 'coerce', **kwargs)
 
         if inplace:
-            original_columns = self.df[self.columns].copy()
-            
-            # first converting all data to numerical, and non-numerical get converted to NaN
-            self.df[self.columns] = self.df[self.columns].apply(pd.to_numeric, errors = 'coerce')
             return None
-
         else:
-            df_copy = self.df.copy()
-            # first converting all data to numerical, and non-numerical get converted to NaN
-            df_copy[self.columns] = self.df[self.columns].apply(pd.to_numeric, errors='coerce')
+            return self.df
 
-            return df_copy
 
 
