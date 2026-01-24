add11c46577f0f8c6d6fb88801234bc695303c58
TheDucky-2 <datalab.project@protonmail.com>
2026-01-24 18:34:46 +0530

    Deleted data visualizer module from init py

--- datalab/tabular/data_visualization/__init__.py
+++ datalab/tabular/data_visualization/__init__.py
@@ -1,7 +1,6 @@
-from .DataVisualizer import DataVisualizer
 from .CategoricalVisualizer import CategoricalVisualizer
 from .NumericalVisualizer import NumericalVisualizer
 from .MissingnessVisualizer import MissingnessVisualizer
 
 
-__all__ = ['DataVisualizer', 'CategoricalVisualizer', 'NumericalVisualizer', 'MissingnessVisualizer']
+__all__ = ['CategoricalVisualizer', 'NumericalVisualizer', 'MissingnessVisualizer']
