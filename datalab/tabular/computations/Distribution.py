790b658fcc4eb1cfb027fa6738fa8ee8f47e023e
TheDucky-2 <datalab.project@protonmail.com>
2026-01-26 18:48:16 +0530

    Fixed bugs, added docstrings and made outputs type consistent in Distribution module

--- datalab/tabular/computations/Distribution.py
+++ datalab/tabular/computations/Distribution.py
@@ -28,77 +28,95 @@ class Distribution(Computation):
         if columns is None:
             self.columns = self.df.columns.tolist()
         else:
-
             self.columns = [column for column in columns if column in self.df.columns] 
     
-    def compute_histogram(self, n_bins: int =30, density: bool =False) -> pd.DataFrame:
+    def compute_histogram(self, n_bins: int =30, density: bool =False, df_axis: int=1) -> pd.DataFrame:
         """
-        Pre-Compute histogram for each numerical column of your DataFrame.
+        Pre-Computes a histogram.
 
         Parameters
         -----------
         n_bins : int, optional
             Number of bins to use in the histogram, by default 30.
+
+        df_axis: int, optional
+            Whether you wish to see a concatenated dataframe column wise or row wise, default is 1.
+
+            Available options:
+
+            - 0 : Shows a concatenated dataframe row wise.
+            - 1 : Shows a concatenated dataframe column wise (default).
         
         density :bool, optional 
             Density is used when we want to see histogram in terms of proabability distributions like normal, exponential or uniform and all, by default False.
             
             If False, 
                 It usually shows how many values fall in each bin
+                
             If True, 
                 It usually shows how likely values are to fill within each range - i.e., the probability density.
                 The total area under the histogram equals to 1.
 
         Returns
         --------
-            dict 
-            A dictionary of of each column as key, and precomputed bins with bin_centers and counts of the histogram for that column as value
+        pd.DataFrame
+            A pandas DataFrame 
     
         Usage Recommendation
         --------------------
-            Use this function to convert columns into numeric values for later calculations.
+            Use this function if you want a pre-computed histogram for calculations.
         
         Considerations
         ---------------
-            This function converts non-convertible values into np.nan (Not a Number) by default.
+            This function returns a Concatenated DataFrame of counts, bin_edges and bin_centers of a histogram.
 
-        Examples:
-        
+        Example
+        ---------
         >>> Distribution(df).compute_histogram(n_bins=50) 
 
         >>> Distribution(df).compute_histogram(density=True)
         """ 
+
+        if not isinstance(n_bins, int):
+            raise TypeError(f'n_bins (number of bins) must be an int, got {type(n_bins).__name__}')
+
+        if not isinstance(density, bool):
+            raise TypeError(f'density must be a bool, got {type(density).__name__}')
+
+        if not isinstance(df_axis, int):
+            raise TypeError(f'df_axis must be a bool, got {type(df_axis).__name__}')
+
+        if df_axis not in [0, 1]:
+            raise ValueError(f"df_axis must either be 0 (for combining df row-wise) or 1 (for combining df column-wise), got {df_axis}")
+
         import numpy as np
 
-        computed_bins = {}
+        counts_dict = {}
+        bin_edges_dict = {}
+        bin_centers_dict = {}
 
-        for column in self.df[self.columns]:
-
-            max_value, min_value = self.df[column].max(), self.df[column].min()
-
-            # Computing bin boundaries
-
-            bin_boundaries = np.linspace(min_value, max_value, n_bins+1) # taking +1 to include last element as part of the boundary
-
-            # Computing occurrence counts of values falling between bin boundaries
-
-            counts, _ = np.histogram(self.df[column], bins = bin_boundaries, density=density)
-
-            # Computing center values of the bins
-
-            ## # bin_boundaries[:-1] is the beginning of boundary 
-            ## # bin_boundaries[1:] is the end of boundary
-            bin_centers = 0.5 * (bin_boundaries[1:] + bin_boundaries[:-1]) 
-
-            computed_bins[column] = pd.DataFrame({
-                
-                    'bin_centers' : bin_centers,
-                    'counts' : counts
-                } )
+        for col in self.df[self.columns]:
             
-        return computed_bins
+            # computing counts and bin edges
+            counts, bin_edges = np.histogram(self.df[col], bins = n_bins, density=density)
 
-    def raw_kurtosis(self) -> pd.Series :
+            # computing bin centers
+            bin_centers = 0.5 * (bin_edges[1:] + bin_edges[:-1])
+
+            counts_dict[col] = counts
+            bin_edges_dict[col] = bin_edges
+            bin_centers_dict[col] = bin_centers
+
+        counts_df = pd.DataFrame(counts_dict)
+        bin_edges_df = pd.DataFrame(bin_edges_dict)
+        bin_centers_df = pd.DataFrame(bin_centers_dict)
+
+        # returning a concatenated df
+        histogram_df = pd.concat({"counts": counts_df, "bin_edges" : bin_edges_df, "bin_centers" : bin_centers_df}, axis=df_axis, ignore_index=False)
+
+        return histogram_df
+
+    def raw_kurtosis(self) -> pd.DataFrame :
         """
         Computes the raw kurtosis for each numerical column of your DataFrame.
 
@@ -112,31 +130,36 @@ class Distribution(Computation):
             - kurtosis ~ 3 means the data is close to normal.
             - kurtosis < 3 means the data has light tails (fewer outliers).
 
-        Formula:
-
+        Formula
+        --------
             raw_kurtosis = (average of (x - mean)^4) / (variance^2)
 
-        Returns:
-            pd.Series
-            A pandas series of raw kurtosis values for each numerical column of your DataFrame
+        Returns
+        --------
+        pd.Series
+            A pandas Series
 
-        Usage Recommendation:
+        Usage Recommendation
+        ---------------------
             1. Use raw kurtosis if you are comparing several distributions mathematically.
             2. Use excess kurtosis if you are comparing to a normal distribution
-        """ 
 
+        Example
+        ---------
+        >>> Distribution(df).raw_kurtosis()
+        """ 
         n = len(self.df)
 
-        df_mean = Statistics(self.df).mean()
-        df_std_dev = Statistics(self.df).standard_deviation()
+        mean = Statistics(self.df).mean()
+        std_dev = Statistics(self.df).standard_deviation()
 
-        fourth_central_moment = (((self.df - df_mean)**4).sum())/n
+        fourth_central_moment = (((self.df - mean)**4).sum())/n
 
-        raw_kurtosis = fourth_central_moment / ((df_std_dev)**4)
+        raw_kurtosis = fourth_central_moment / ((std_dev)**4)
 
-        return raw_kurtosis
+        return raw_kurtosis.to_frame(name='raw_kurtosis')
 
-    def excess_kurtosis(self) -> pd.Series :
+    def excess_kurtosis(self) -> pd.DataFrame :
         """
         Computes the excess kurtosis for each numerical column of your DataFrame.
 
@@ -163,100 +186,128 @@ class Distribution(Computation):
         ---------------------
             1. Use excess kurtosis if you are comparing to a normal distribution
             2. Use raw kurtosis if you are comparing several distributions mathematically.
+        
+        Example
+        ---------
+        >>> Distribution(df).excess_kurtosis()
         """
 
         raw_kurtosis = Distribution(self.df).raw_kurtosis()
 
-        excess_kurtosis = raw_kurtosis - 3
+        excess_kurtosis = raw_kurtosis['raw_kurtosis'] - 3
 
-        return excess_kurtosis
+        return excess_kurtosis.to_frame(name='excess_kurtosis')
 
-    def compute_KDE(self, bandwidth_method ='silverman')-> pd.DataFrame:
+    def compute_kde(self, bandwidth_method:str ='silverman',n_bins: int =30, density: bool =False)-> pd.DataFrame:
+
         import numpy as np
         import pandas as pd
         from scipy.signal import fftconvolve
         """
-        Computes the KDE (Kernel Density) for each numerical column of the
-        DataFrame.
+        Computes the KDE (Kernel Density Estimation).
 
-        Intuition:
-        ----------
+        KDE is just a smoothened histogram where bars are converted into curves
+        Instead of showing counts per bin, KDE uses a smooth “kernel” (a bell-shaped curve) to estimate the probability distribution.
 
-            KDE is just a smoothened histogram where bars are now curves
-            Instead of showing counts per bin, KDE uses a smooth “kernel” (a bell-shaped curve) to estimate the probability distribution.
-
-        Formula:
+        Formula
         --------
+            K(x)= e^(-x^2/2(h^2)) , where h is the Bandwidth that controls how wide the bump should be
 
-            K(x)= e^-x^2/2(h^2) , where h is the Bandwidth that controls how wide the bump should be
-
-        Parameters:
+        Parameters
         -----------
-
-        Optional:
-
-            bandwidth_method: str (default is 'Silverman')
+        bandwidth_method: str, optional
+            Bandwidth method decides how wide each bump is when smoothing data for KDE, default is 'silverman'.
 
             Bandwidth methods available:
 
-            For Normal Distribution:
+            For Normal Distribution
 
-                - 'silverman' : 1.06 * std * (n ** (-1/5))
-                - 'scott':              std * (n ** (-1/5)) , same as silverman without the optimized constant 1.06
+            - 'silverman' : 1.06 * std * (n ** (-1/5)) 
+            - 'scott':  std * (n ** (-1/5)) , same as silverman without the optimized constant 1.06
 
-            For Skewed Distribution:
+            For Skewed Distribution
 
-                - 'robust': (IQR/1.349) * (n ** (-1/5)) , uses IQR instead of standard deviation
+            - 'robust': (IQR/1.349) * (n ** (-1/5)) , uses IQR instead of standard deviation
 
-        Returns:
+        n_bins : int, optional
+            Number of bins to use in the histogram, by default 30.
+        
+        density :bool, optional 
+            Density is used when we want to see histogram in terms of probability instead of raw counts, by default False.
+            
+            If False, 
+                It usually shows count of values that fall in each bin
+                
+            If True, 
+                It usually shows how likely values are to fill within each range - i.e., the probability density.
+                The total area under the histogram equals to 1.
+
+        Returns
         --------
-            pd.DataFrame
-            A pandas DataFrame of KDE values
+        pd.DataFrame
+            A pandas DataFrame 
 
-        Usage Recommendation:
+        Usage Recommendation
+        --------------------
             1. Use this function when you want to compute KDE values, for visualization or analysis
             2. Use 'silverman' or 'scott' as bandwidth method if your data has normal distribution.
             3. Use 'robust' if your data has skewed distribution.
 
-        Considerations:
+        Considerations
+        ---------------
             This function calculates bandwidth (width of bump) separately for each column, which is why it is a bit slower.
+        
+        Example
+        -------
+        >>> Distribution(df).compute_kde()
+
+        >>> Distribution(df).compute_kde('scott', n_bins = 50)
         """
+        from .Statistics import Statistics
+
         n = len(self.df)
+
         std_dev =Statistics(self.df).standard_deviation()
 
-        IQR = Statistics(self.df).IQR()
+        IQR = Statistics(self.df).iqr()
+
+        histogram = Distribution(self.df).compute_histogram(n_bins=n_bins, density=density)
         
         KDE_dict = {}
 
         for column in self.df[self.columns]:
 
-
             if bandwidth_method == 'scott':
 
-                Bandwidth = std * (n ** (-1/5))
+                bandwidth = std_dev[column] * (n ** (-1/5))
                 
             elif bandwidth_method == 'silverman':
 
-                Bandwidth = 1.06 * std * (n ** (-1/5))
+                bandwidth = 1.06 * std_dev[column] * (n ** (-1/5))
 
             else:
-            # robust scott, uses IQR instead of standard deviation
-                Bandwidth = (IQR/1.349) * (n ** (-1/5))
+                # robust scott, uses IQR instead of standard deviation
+                bandwidth = (IQR[column]/1.349) * (n ** (-1/5))
+
+            bw = float(bandwidth)
+
+            if not np.isfinite(bw) or bw<=0:
+                raise ValueError(f'Invalid bandwidth for column {column}: {bw}')
 
             # importing the Distrubution computation and using bin centers to compute KDE since it is easier that way
-            bin_centers = Distribution(self.df).compute_histogram()[column]['bin_centers']
-            counts = Distribution(self.df).compute_histogram()[column]['counts']
+            bin_centers = histogram['bin_centers'][column].dropna()
+            counts = histogram['counts'][column].dropna()
             
             # calculating grid size using bin_centers 
             grid_size = len(bin_centers)
 
-            #### CALCULATING KERNEL (Bump) FUNCTION 
-
             # creating an artifical axis around zero, for kernel
-            x_axis = np.linspace(-3 * Bandwidth[column], 3 * Bandwidth[column], grid_size)
+            x_axis = np.linspace(-3 * bw, 3 * bw, grid_size)
+
+            x_axis = np.asarray(x_axis, dtype=float)
             
             # creating kernel for making the curve
-            kernel = np.exp((-x_axis**2)/(2 * Bandwidth[column]**2))
+            kernel = np.exp((-x_axis**2)/(2 * bw**2))
             
             # making sure all values add up to 1
             kernel/=kernel.sum()
@@ -267,30 +318,3 @@ class Distribution(Computation):
             KDE_dict[column] = KDE
 
         return pd.DataFrame(KDE_dict)
-
-    def skewness(self):
-        """
-        Computes the skewness (if data is being pulled in one direction).
-
-        Skewness:
-        
-        - Values near 0 means the data is roughly symmetric.
-        - Positive values indicate that data is being pulled towards right direction.
-        - Negative values indicate taht data is being pulled towards left direction.
-
-        Formula
-        -------
-            skewness = (1/n) * sum of ((x - mean of x)/std dev of x)**3
-
-        Returns
-        -------
-            pd.Series
-            A pandas Series
-        
-        Usage Recommendation
-        ---------------------
-            Use this function for checking the shape of your data (whether it follows normal distribution or non-normal distribution).
-        """ 
-
-        return self.df.skew()
-
