759fc3ca1a5636dc0e5dd6c8787c5f63ccabec97
TheDucky-2 <datalab.project@protonmail.com>
2026-01-26 13:32:55 +0530

    FIxed bugs, refactored code for API consistency, and added docstrings

--- datalab/tabular/data_diagnosis/NumericalDiagnosis.py
+++ datalab/tabular/data_diagnosis/NumericalDiagnosis.py
@@ -1,6 +1,5 @@
-"""Diagnoses the Numerical Data in your dataset"""
+"""Diagnoses the Numerical Data in your DataFrame"""
 
-from ..computations import Distribution
 from ..utils.Logger import datalab_logger
 
 from pathlib import Path
@@ -8,22 +7,11 @@ import pandas as pd
 
 logger = datalab_logger(name = __name__.split('.')[-1])
 
-def convert_numpy_scalars_to_python(number)-> dict[str, dict]:
-    import numpy as np
-
-    if isinstance(number, np.integer):
-        return int(number)
-    
-    elif isinstance(number, np.floating):
-        return float(number)
-    
-    else:
-        return number
-
 class NumericalDiagnosis:
 
     def __init__(self, df: pd.DataFrame, columns:list = None):
-        """Initializing the Numerical Diagnosis.
+        """
+        Initializing the Numerical Diagnosis.
 
         Parameters
         -----------
@@ -32,7 +20,7 @@ class NumericalDiagnosis:
             A pandas dataframe you wish to diagnose.
 
         columns: list, optional
-            A list of columns you wish to apply numerical diagnosis on, by default None
+            A list of columns you wish to apply numerical diagnosis on, by default None.
         """
 
         if not isinstance(df, pd.DataFrame):
@@ -44,25 +32,25 @@ class NumericalDiagnosis:
         self.df = df.select_dtypes(include='number')
 
         if columns is None:
-            self.columns = df.columns.tolist()
+            self.columns = self.df.columns.tolist()
         else:
             self.columns = [column for column in columns if column in self.df.columns]
 
         logger.info('Numerical Diagnosis initialized.')
             
-    def check_sparsity(self, value: int|float = 0) -> pd.Series:
-        """Checks the ratio of a specific number (usually 0) present in the selected column of the DataFrame.
+    def check_sparsity(self, value: int|float = 0) -> dict[str, float]:
+        """
+        Checks the ratio of occurrence of a specific number (usually 0).
 
         Parameters
         -----------
-
-        value: int or float (default is 0)
-            An integer or a decimal number
+        value: int or float, optional
+            An integer or a decimal number, by default 0.
 
         Returns
         --------
-        pd.Series
-            A pandas Series of the columns passed in, along with the ratio that value appears in the Dataframe. 
+        dict[str, float]
+            A dictionary of column names and percentage of occurrence of a particular number.
             
         Usage Recommendation
         --------------------
@@ -77,34 +65,25 @@ class NumericalDiagnosis:
         Example
         --------
         >>> NumericalCleaner(df).check_sparsity()
-
-            Output: age                  0.000000
-                    income               0.000000
-                    expenses             0.000000
-                    savings              0.000000
-                    loan_amount          0.000000
-                    credit_score         0.000000
-                    num_of_dependents    0.147308
-                    years_at_job         0.021907
-                    risk_score           0.335651           <- 33.5% values in column 'risk_score' are 0 (or 0.00).
-                    dtype: float64
+        >>> NumericalCleaner(df).check_sparsity(5)
         """
+        sparsity_dict = {
+            col: round(float((((self.df[col] == 0).sum())/len(self.df[col]))* 100), 2)
+            for col in self.df[self.columns]}
 
-        # getting the ratio of value present in the column
-        sparsity = self.df[self.columns].apply(lambda column: ((column == value).sum()/len(column))*100)
-
-        # Shows occurence of each value in percent
-        print(f'\nOccurrence of {value} in each column of the dataframe (in %)')
+        # Shows occurence of each value in percentage
+        logger.info(f'Occurrence of {value} in each column of the dataframe (in %)')
         
-        return sparsity
+        return sparsity_dict
 
     def detect_outliers(self, method: str ='IQR')-> dict[str, pd.Series]:
-        """Detects outliers in one or multiple Numerical columns of the DataFrame.
+        """
+        Detects outliers in one or multiple Numerical columns of the DataFrame.
 
         Parameters
         -----------
         method : str (default is 'IQR')
-            Method using which you wish to detecting outliers.
+            Method using which you wish to detect outliers.
 
             Supports:
             
@@ -113,7 +92,7 @@ class NumericalDiagnosis:
 
         Returns
         -------
-        pd.Series
+        dict[str, pd.Series]
             A pandas Series of columns with outliers.
             
         Usage Recommendation
@@ -125,53 +104,31 @@ class NumericalDiagnosis:
         -------- 
         >>> NumericalDiagnosis(df).detect_outliers()
         """
+        from datalab import Outliers
+
         outliers_dict  = {}
 
         for col in self.df[self.columns]:
             
             if method == 'IQR':
                 
-                # the 1st quartile 
-                q1 = self.df[col].quantile(0.25)
+                outliers_dict[col] = Outliers(self.df).iqr_outliers()[col]
 
-                # the 3rd quartile
-                q3 = self.df[col].quantile(0.75)
+            elif method == 'z_score':
 
-                # the remaining 50% of data in the middle
-                IQR = q3 - q1
-
-                # we are subtracting 1.5 times of 50% of data from the 25% quantile, which means values would be very high in negative
-                lower_bound = q1 - (1.5 * IQR)
-
-                # we are add 1.5 times of 50% of data to the 75% quantile, which means values would be very high in positive
-                upper_bound = q3 + (1.5 * IQR)
-
-                # getting outliers that are numbers which are either lower than lower boundary or higher than higher boundary
-                outliers_dict[col] = self.df[((self.df[col]) < lower_bound) | ((self.df[col]) > upper_bound)][col]
-
-            elif method == 'z-score':
-                mean = self.df[col].mean()
-                std = self.df[col].std()
-
-                z_score = np.abs((self.df[col] - mean)/std)
-                
-                # getting outliers which have a z-score of more than 3 
-                outliers = (self.df[col][z_score>3])
-
-                outliers_dict[col] = outliers
-
-        # getting only the columns where outliers actually exist
-        outliers_dict = {col: df for col, df in outliers_dict.items() if not df.empty}
-        print("\nUses IQR method by default. If you want to detect outliers using z-score, use method = 'z-score'")
+                outliers_dict[col] = Outliers(self.df).zscore_outliers()[col]
             
         return outliers_dict
-    def check_skewness(self)-> pd.Series:
-        """Checks the skewness in each column of the DataFrame.
+
+    def check_skewness(self)-> dict[str, float]:
+        """
+        Checks the skewness in each column of the DataFrame.
 
         Returns
         --------
-        pd.Series
-            A pandas Series of all columns of the DataFrame with skewness values.
+        dict[str, float]
+
+            A dictionary of all columns of the DataFrame with skewness values.
             
         Usage Recommendation
         ---------------------
@@ -182,99 +139,140 @@ class NumericalDiagnosis:
         ------- 
         >>> Diagnosis(df).check_skewness()
         """
-        
-        return self.df.skew()
+        from ..computations.Distribution import Distribution
 
-    def check_zero_variance(self):
-            
-        if Statistics(self.df).variance() == 0:
-            return f'These columns have zero variance: {column}'
-        
-        elif Statistics(self.df).variance() != 0:
-            return f'No columns have zero variance'
-    
-    def check_distribution(self, skewness_threshold: int|float=1,kurtosis_threshold: int|float=2)-> pd.Series :
-        """Checks whether the distribution of data is Normal or Non-Normal, for each column of DataFrame.
+        skewness = Distribution(self.df).skewness()
+
+        skewness_dict = {col: round(float(skewness[col]), 4)for col in self.df[self.columns]}
+
+        return skewness_dict
+
+    def check_kurtosis(self, kurtosis_type='raw')-> dict[str, float]:
+        """
+        Checks the kurtosis value in each column of the DataFrame.
 
         Parameters
-        -----------
-        skewness_threshold: int or float (default is 1)
+        ----------
+        kurtosis_type : str, optional
+            The type of kurtosis you would like to compute, default is 'raw'.
 
-            The absolute value for skewness(|skewness|) below which the data distribution is considered to be a Normal Distribution.
+            Types supported:
             
-        kurtosis_threshold: int or float( default is 2)
-
-            The absolute value for kurtosis(|kurtosis|) below which the data distribution is considered to be a Normal Distribution.
+            - 'raw'
+            - 'excess'
 
         Returns
         --------
-        pd.Series
-            A pandas Series of column names and their classification as 'Normal' vs 'Non-Normal'. 
+        dict[str, float]
+            A dictionary of all columns of the DataFrame with skewness values.
             
         Usage Recommendation
         ---------------------
-            1. Use this function only for Exploratory Data Analysis.  
-            2. Use this function before exploring strategies for handling missing data and data preprocessing.
-            
+            1. Use this function when you want to check how unevenly data is distributed around the mean (skewness).
+            2. Use only for numerical columns.
+
         Example
-        -------
-        >>> Diagnosis(df).check_distribution()
-                
-            age                      Normal Distribution
-            income               Non-Normal Distribution
-            expenses             Non-Normal Distribution
-            savings              Non-Normal Distribution
-            loan_amount          Non-Normal Distribution
-            credit_score             Normal Distribution
-            num_of_dependents        Normal Distribution
-            years_at_job             Normal Distribution
-            risk_score               Normal Distribution
-            dtype: object
+        ------- 
+        >>> Diagnosis(df).check_skewness()
+        """ 
+        if not isinstance(kurtosis_type, str):
+            raise TypeError(f'kurtosis type must be a string, got {type(kurtosis_type).__name__}')
+
+        if kurtosis_type not in ['raw', 'excess']:
+            raise ValueError("Available kurtosis types: 'raw' or 'excess'.")
+
+        from ..computations import Distribution
+
+        raw_kurtosis = Distribution(self.df).raw_kurtosis()
+        excess_kurtosis = Distribution(self.df).excess_kurtosis()
+
+        if kurtosis_type == 'raw':
+            kurtosis_dict = {col: round(float(raw_kurtosis[col]), 4)for col in self.df[self.columns]}
+
+        if kurtosis_type == 'excess':
+            kurtosis_dict = {col: round(float(excess_kurtosis[col]), 4)for col in self.df[self.columns]}
+
+        return kurtosis_dict
+
+    def check_zero_variance(self)-> dict[str, float]:
         """
-        import numpy as np
-        
-        # getting Skewness from numerical diagnosis class and kurtosis calculation from Distribution class
-        skewness = NumericalDiagnosis(self.df).check_skewness()
-        kurtosis = Distribution(self.df).excess_kurtosis()
-
-        distributions = {}
-
-        for column in self.df[self.columns]:
-        
-            if np.abs(skewness[column]) < skewness_threshold and np.abs(kurtosis[column]) < kurtosis_threshold:
-                distributions[column] = 'Normal Distribution'
-
-            else:
-                distributions[column] = 'Non-Normal Distribution'
-            
-        distributions= pd.Series(distributions)
-
-        return distributions
-
-    def show_minmax(self):
-        """Show minimum and maximum values prexentt in each Numerical column of the DataFrame.
+        Checks if the variance of a column is 0.
 
         Returns
         --------
-        dict
-            A dictionary of column names and min-max values present in that column.
+        dict[str, float]
+
+            A dictionary of all columns of the DataFrame with their variance
+            
+        Usage Recommendation
+        ---------------------
+            1. Use this function when you want to check if all the values in a column are exactly the same.
+            2. Use only for numerical columns.
 
         Example
-        --------
-
-        >>> NumericalDiagnosis(df).show_min_max()
+        ------- 
+        >>> Diagnosis(df).check_zero_variance()
         """
-        range_dict = {}
-    
-        for column in self.df[self.columns]:
-            min = self.df[column].min()
-            max = self.df[column].max()
-            
-            # applying the method to convert numpy scalars into python numbers.
-            range_dict[column] = {
-                'min': convert_numpy_scalars_to_python(min),
-                'max': convert_numpy_scalars_to_python(max)
-            }
+        from ..computations.Statistics import Statistics
+        
+        zero_variance_dict = {}
 
-        return range_dict
+        for col in self.df[self.columns]:
+
+            variance = Statistics(self.df).variance()[col]
+
+            if (variance == 0):
+                zero_variance_dict[col] = variance
+            else:
+                zero_variance_dict[col] = round(float(variance), 3)
+
+        return zero_variance_dict
+
+    def check_distribution(self, skewness_threshold:int|float=1, kurtosis_threshold: int|float=2)-> dict[str, str]:
+        """
+        Classifies whether the distribution of data is normal or non-normal, based on skewness and kurtosis values.
+
+        Parameters
+        ----------
+        skewness_threshold : int or float, optional
+            Threshold of skewnes values above which distribution would be considered Non-Normal, default is |1|.
+
+        kurtosis_threshold : int or float, optional
+            Threshold of kurtosis values above which distribution would be considered Non-Normal, default is |2|.
+
+        Returns
+        --------
+        dict[str, str]
+            A dictionary of all columns with their classification as Normal or Non-Normal Distribution.
+            
+        Usage Recommendation
+        ---------------------
+            1. Use this function when you want to check which columns follow a Normal-Distribution and which ones follow a Non-Normal Distribution.
+            2. Use this function explicitly for EDA (Exploratory Data Analysis).
+            2. Use only for numerical columns.
+
+        Example
+        ------- 
+        >>> NumericalDiagnosis(df).check_distribution()
+        """ 
+        if not isinstance(skewness_threshold, (int, float)):
+            raise TypeError(f'skewness threshold must be an int or float, got {type(skewness_threshold).__name__}')
+
+        if not isinstance(kurtosis_threshold, (int, float)):
+            raise TypeError(f'skewness threshold must be an int or float, got {type(skewness_threshold).__name__}')
+
+        from ..computations.Distribution import Distribution
+
+        distribution_dict = {}
+        
+        skewness = Distribution(self.df).skewness()
+        kurtosis = Distribution(self.df).excess_kurtosis()
+
+        for col in self.df[self.columns]:
+            if (skewness.abs()[col] > skewness_threshold) & (kurtosis.abs()[col] > kurtosis_threshold):
+                distribution_dict[col] = 'Non-Normal Distribution'
+            else:
+                distribution_dict[col] = 'Normal Distribution'
+
+        return distribution_dict
 
