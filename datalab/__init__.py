6174707fd9b978b93a5b76c1df8e6db28ac164be
TheDucky-2 <datalab.project@protonmail.com>
2026-02-01 17:55:06 +0530

    Bumped version to v0.1.0b6

--- datalab/__init__.py
+++ datalab/__init__.py
@@ -1,11 +1,11 @@
 """
 <img src="assets/DataLab_logo.png" alt="DataLab logo" height: "80" style="display:block; margin: 20px auto;">
 
-"DataLab v0.1.0b5 - Beta Pre-release
+"DataLab v0.1.0b6 - Beta Pre-release
 
 A stabilization release focused on API reliability, type safety, improved tests, and clearer documentation."
 """
-__version__ = '0.1.0b5'
+__version__ = '0.1.0b6'
 
 import importlib
 
