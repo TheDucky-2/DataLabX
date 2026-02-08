884ebe8a2095816980ca2834cd81eca97411141f
TheDucky-2 <datalab.project@protonmail.com>
2026-02-08 14:56:06 +0530

    Fixed bug that was causing import issues in non jupyter notebook environments

--- datalab/tabular/data_visualization/__init__.py
+++ datalab/tabular/data_visualization/__init__.py
@@ -1,3 +1,18 @@
+def set_visualization_backend():
+    import matplotlib
+
+    try:
+        env_shell = get_ipython().__class__.__name__
+
+        if env_shell == "ZMQInteractiveShell":
+            matplotlib.use('module://matplotlib_inline.backend_inline')
+        else:
+            matplotlib.use('agg')
+    except NameError:
+        matplotlib.use('agg')
+
+set_visualization_backend()
+
 from .CategoricalVisualizer import CategoricalVisualizer
 from .NumericalVisualizer import NumericalVisualizer
 from .MissingnessVisualizer import MissingnessVisualizer
