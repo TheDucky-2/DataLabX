6e6e473c5bb86034dfddf126b110edd4a121deb9
TheDucky-2 <datalab.project@protonmail.com>
2026-05-06 11:33:17 +0530

    Added measure decorator for measuring performance

--- datalabx/tabular/utils/_decorators.py
+++ datalabx/tabular/utils/_decorators.py
@@ -1,24 +1,36 @@
 """Includes internal decorators used within DataLabX modules"""
 
 from functools import wraps
+import time
+from .Logger import datalabx_logger
 
+logger = datalabx_logger(name = "DataLabX")
 
-## decorator for handlingsetting and resetting of index column
 def handle_index(func):
+    """Decorator for handling setting and resetting of index column"""
     @wraps(func)
     def wrapper(self, *args, **kwargs):
 
         input_df = self.df.reset_index()
-
         result = func(self, *args, **kwargs)
 
         for col in result:
             for key in result[col]:
-
                 self.df = result[col][key]
-
                 if 'index' in self.df.columns:
                     self.df.set_index("index", inplace=True)
 
         return result
     return wrapper
+
+def measure(func):
+    @wraps(func)
+    def wrapper(self, *args, **kwargs):
+        start = time.perf_counter()
+        result = func(self,*args, **kwargs)
+        end = time.perf_counter()
+        logger.info(f"Time taken: {(end-start)*1000:.3f} ms")
+        return result
+
+    return wrapper
+
